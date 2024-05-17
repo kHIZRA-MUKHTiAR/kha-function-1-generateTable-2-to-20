604ced60b5aba2ae5d5f5274b48ff3eeef24d70c
kHIZRA-MUKHTiAR <161032197+kHIZRA-MUKHTiAR@users.noreply.github.com>
2024-05-17 06:21:54 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+ 
+function generateTable(num:number ) {
+    console.log(num + " * 1 = " + num*1);
+    console.log(num + " * 2 = " + num*2);
+    console.log(num + " * 3 = " + num*3);
+    console.log(num + " * 4 = " + num*4);
+    console.log(num + " * 5 = " + num*5);
+    console.log(num + " * 6 = " + num*6);
+    console.log(num + " * 7 = " + num*7);
+    console.log(num + " * 8 = " + num*8);
+    console.log(num + " * 9 = " + num*9);
+    console.log(num + " * 10 = " + num*10);
+   }
+    generateTable(2)
+    console.log("");
+    generateTable(3)
+    console.log("");
+    generateTable(4)
+    console.log("");
+    generateTable(5)
+    console.log("");
+    generateTable(6)
+    console.log("");
+    generateTable(7)
+    console.log("");
+    generateTable(8)
+    console.log("");
+    generateTable(9)
+    console.log("");
+    generateTable(10)
+    console.log("");
+    generateTable(11)
+    console.log("");
+    generateTable(12)
+    console.log("");
+    generateTable(13)
+    console.log("");
+    generateTable(14)
+    console.log("");
+    generateTable(15)
+    console.log("");
+    generateTable(16)
+    console.log("");
+    generateTable(17)
+    console.log("");
+    generateTable(18)
+    console.log("");
+    generateTable(19)
+    console.log("");
+    generateTable(20)
+    console.log("")
